421a4136e184a6da95e175cf14f730c4d74c1732
y-syo <mmoussou@student.42angouleme.fr>
2025-04-28 03:28:23 +0200

    「✨」 feat: did a lot of things ! css is clean and now follow the new style i went for, added things in the other menus (except the music one), and added the funny random banner image hehe

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
+	const images = [
+		"https://media1.tenor.com/m/i-EHxKJYStIAAAAC/james-wilson-house.gif",
+		"https://media1.tenor.com/m/WAKLsy9B6FsAAAAd/tekken-tekken-8.gif",
+		"https://media1.tenor.com/m/K58N07lKvpAAAAAC/rashid-street.gif",
+		"https://media1.tenor.com/m/sWIAJ_IVk6kAAAAd/my-honest-reaction-my-honest-reaction-meme.gif",
+		"https://media1.tenor.com/m/banj382oaJMAAAAd/horse-horse-yapping.gif",
+		"https://media1.tenor.com/m/9QT5PUtMUeIAAAAd/spectre-osu-players.gif",
+		"https://media1.tenor.com/m/mKjSo7AS9vAAAAAd/onimai-mahiro.gif",
+		"https://media1.tenor.com/m/v8nb7KZ2L7sAAAAd/michiru-kagemori-michiru.gif",
+		"https://media1.tenor.com/m/3lAgnX3yF5QAAAAd/mizuki-akiyama.gif",
+		"https://media1.tenor.com/m/9zaRzkks96oAAAAd/nene-kusanagi-nene-pjsk.gif",
+		"https://media1.tenor.com/m/PE5ruuKARBIAAAAd/anime-love-live.gif"
+	];
+	const randomIndex = Math.floor(Math.random() * images.length);
+	document.getElementById('image-maincontent').src = images[randomIndex];
+
 	const tabButtons = document.querySelectorAll(".window-tablist-button");
 	const contentSections = document.querySelectorAll(".window-menuarticle");
 
